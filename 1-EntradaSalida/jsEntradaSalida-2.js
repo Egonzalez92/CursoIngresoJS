6f113010bfc24eada1ccfb947a174dff24796913
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-02-08 20:37:17 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,24 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
-	
+{	
+	// defino una variable
+	var nombre;
+	alert(nombre);
+	nombre=prompt("ingrese su nombre","emmanuel gonzalez");
+	alert(nombre);
+
+
+
+	/*
+	nombre="Emmanuel";
+	alert(nombre);
+	alert("nombre");
+	Si aparece la palabra "undefined" es porque la variable no esta definida.
+	El alert sirve para mostrar el contenido de la variable
+
+	*/
+
+	// var person = prompt("Please enter your name", "Harry Potter");
 }
 
