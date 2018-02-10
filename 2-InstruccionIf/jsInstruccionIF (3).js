7fede840d9b53e7185dcf8971339195ca417720a
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-02-09 22:05:25 -0300

    Ejercicios Realizados 1,2,3,4,5
    Se realizaron dichos ejercicios.

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var	edad;
 
-}//FIN DE LA FUNCIÓN
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	if (edad>="18") 
+
+	{alert("Es mayor de edad")}
+
+	
+	if (edad<=17) {alert("Es menor de edad")}
+
+	}//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+	var	edad;
 
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	if (edad>=13 && edad<=17)
+
+		{alert("Es adolescente")}
+	
+	if (edad>=18 && edad<=99)
+
+		{alert("No son adolescentes")}
 
 }//FIN DE LA FUNCIÓN
