a41892c9d4ccb16c645c189a6ddc1c5690e05ae5
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-02-16 07:39:11 -0300

    Ejercicios 6,7,9
    Falta realizar 8 y 10. Y consultar en el 7.

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,18 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var	edad;
+	var estadocivil;
+	
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+	estadocivil=document.getElementById('estadoCivil').value;
+
+	if (edad<=18)
+
+		alert("Es muy pequeño para NO ser soltero")
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,8 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	
+	var aleatorio= Math.round(Math.random()*10);
+alert("Número aleatorio entre 1 y 10:  "+ aleatorio);
+
+}//FIN DE LA FUNCIÓN
