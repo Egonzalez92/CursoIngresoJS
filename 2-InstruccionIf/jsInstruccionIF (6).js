789c9de6b94c09cae84ee331af099a693af2a3ea
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-02-16 07:41:47 -0300

    Ej.  6, 7,9
    Faltan realizar 8 y 10. Consultar 7.

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,21 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var	edad;
 
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	if (edad>=18)
+
+		{alert("Es mayor de edad")}
+	
+	if (edad>=13 && edad<=17)
+
+		{alert("Es adolescente")}
+
+	else if (edad<13)
+
+		{alert("Es niño")}
 
 }//FIN DE LA FUNCIÓN
