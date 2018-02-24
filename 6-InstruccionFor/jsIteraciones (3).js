1ebd5baade2257262375f274d7f496a865214f69
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-02-23 22:10:16 -0300

    Completo
    Completo del 1 al 4

---
+++ 6-InstruccionFor/jsIteraciones (3).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+
+	var clave=prompt("ingrese el numero clave");
+
+	while(clave!="utn750")
+	{
+		clave=prompt("Reingrese la clave");
+	}
+		
+		document.write("Bienvenido");
+}//FIN DE LA FUNCIÓN
---
+++ 6-InstruccionFor/jsIteraciones (4).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+
+	var numero = prompt("ingrese un número entre 0 y 10.");
+
+	while(numero<0 || numero>10)
+		{
+			numero=prompt("error, ingrese un número entre 0 y 10.")
+		}
+
+			document.getElementById('Numero').value=numero;
+}//FIN DE LA FUNCIÓN
