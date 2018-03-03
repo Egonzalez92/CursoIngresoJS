3d6598b152d8930fd64eb61ab61a721aa05700f3
Egonzalez92 <36281544+Egonzalez92@users.noreply.github.com>
2018-03-03 13:12:42 -0300

    hecho

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -5,57 +5,47 @@ function Mostrar()
 la termperatura del habitat donde vive el animal entre -40 y 40, me van a informar el nombre del animal mas pesado,
 el nombre del animal de la temp mas baja, la cantidad de temperaturas pares que se ingresaron, el promedio del peso
 de todos los animales, la temp maxima y la temp minima */
+var contador=0;
+var numero;
+var cantpares=0;
+var acumulador=0;
+var maximo;
+var minimo;
+var flag=0;
+var seguir=true;
+var promedio;
+var 
 
 
-	var nombreAnimal;
-	var cantdeanimales;
-	var peso=0;
-	var temperatura=0;
-	var tempmax;
-	var tempmin;
-	var promedio;
-	var respuesta='si'
 
-	while(respuesta='si')
+while (seguir==true)
+	{
+		contador++;
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
 
-	{	
-
-		cantdeanimales++;
-		nombreAnimal=prompt("¿Cual es el nombre del animal?");
-		
-		peso=prompt("¿Cual es el peso del animal");
-		peso=parseInt(peso);
-
-		temperatura=prompt("¿Cual es la temperatura de su habitat");
-		temperatura=parseInt(temperatura);
-
-
-		while(isNaN(nombreAnimal))
+		while(numero<0)
 		{
-			nombreAnimal=prompt("Cual es el nombre del animal?");
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		}
+		if(numero%2==0)
+		{
+			cantpares++;
+		}
+	
+		acumulador+=numero;
+		if(flag==false || numero<minimo)
+		{
+			minimo=numero;
+		}
+		if(flag==false || numero>maximo)
+		{
+			maximo=numero;
+			flag=true;
 		}
 
-			while(isNaN(peso) || peso>0)
-			
-			{
-			
-			peso=prompt("¿Reingrese el peso del animal");
-			peso=parseInt(peso);
-				
-			}
-				while(isNaN(temperatura) || temperatura< -41 || temperatura>40)
-				{
-				temperatura=prompt("¿Cual es la temperatura de su habitat");
-				temperatura=parseInt(temperatura);
-				}
-					while(pesoAnimal)
-
-		
-					respuesta=("Desea continuar?");	
 
 	}	
-			
-
-	
+		seguir=confirm("desea continuar?");
 }
-/*Siempre buscar datos.*/
